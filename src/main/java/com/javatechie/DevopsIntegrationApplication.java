4ea4c46a706715cc15ff6a167d7c68dd09c675db
Abhishek Kushawaha <Abhishek.Kushawaha@ibm.com>
2024-05-14 13:18:41 +0530

    change java smaple code

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to javatechie";
+		return "welcome to java saple application";
 	}
 
 	public static void main(String[] args) {
